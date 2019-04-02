445de5e317478fd7d15b231ca0cb26a32b211370
Estudiante <Estudiante@BE454S402C10ING.unal.loc>
2019-04-02 08:47:34 -0500

    EJEMPLO HOLA MUNDO  version 2

--- src/hellow_world/Hellow_world.java
+++ src/hellow_world/Hellow_world.java
@@ -9,15 +9,21 @@ package hellow_world;
  *
  * @author Estudiante
  */
+import java.util.*;
+
 public class Hellow_world {
+
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
         System.out.println("hola mundo"+" 2");
+        Scanner lectura=new Scanner(System.in); 
+        int x = lectura.nextInt();
+        System.out.println("ingreso "+x);
         
+       
     
     
         // TODO code application logic here
